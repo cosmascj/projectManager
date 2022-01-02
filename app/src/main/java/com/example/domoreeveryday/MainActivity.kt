c0f9a21d68783626cf99f271e10a489cccf7e967
cosmascj <cosmascj@gmail.com>
2022-01-02 16:24:14 +0100

    Setting up my Splash screen using the latest andriod Api

--- app/src/main/java/com/example/domoreeveryday/MainActivity.kt
+++ app/src/main/java/com/example/domoreeveryday/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.domoreeveryday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+setKeepVisibleCondition{
+    viewModel.isLoading.value
+}
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
